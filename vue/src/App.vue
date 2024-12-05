03b3f672849b5ef342c85f52b0c6c42e4704762a
Nannahp <nannahofgaard@hotmail.com>
2024-12-05 21:26:47 +0100

    Headings H1 H2 H3

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import axios from "axios";
 
+
 async function fetchKommuner() {
   const response = await axios.get("http://localhost:8080/kommuner?regionskode=1084");
   console.log(response);
 }
 </script>
 
-<template>INDEX</template>
+<template>
+
+  </template>
 
 <style scoped></style>
---
+++ vue/src/components/Headings.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { computed } from 'vue';
+export default defineComponent ({
+  name: "Headings",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+    level: {
+      type: Number,
+      default: 1
+    }
+  },
+  setup(props) {
+        const sizeClass = computed(() => {
+      return {
+        1: 'text-6xl sm:text-8xl',
+        2: 'text-5xl sm:text-7xl',
+        3: 'text-3xl sm:text-5xl',
+      }[props.level]
+    });
+  }
+})
+</script>
+
+<template>
+  <component :is="'h' + level" :class="` text-[#4A4A4A] ${sizeClass}`">
+    <slot />
+  </component>
+</template>
+
+<style scoped>
+
+</style>
