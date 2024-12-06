62d9312a0f899b74e0b06b49b64f40395c01287b
emma673r <90177139+emma673r@users.noreply.github.com>
2024-12-06 14:16:00 +0100

    added test as a route for dev purposes made a FooterComponent.vue changed the way IconButton works because we now use awesome font library the curcle arrow cant be font awesome bc have to be pro to get the outlined version deleted icon compoennts, unecessary now styles.css --> no padding or margin on stuff, style it yourself

--- vue/src/main.ts
+++ vue/src/main.ts
@@ -1,13 +1,12 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import TestZone from "./TestZone.vue";
-import router from './router'; // Adjust path if needed
+import router from "./router"; // Adjust path if needed
 
 //createApp(App).mount('#app')
 //createApp(TestZone).mount("#app");
 
 // Create the Vue app and mount it with the router
 createApp(App)
-    .use(router) // Use the router
-    .mount('#app')
+  .use(router) // Use the router
+  .mount("#app");
---
+++ vue/src/views/Test.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import IconButtonComponent from "../components/IconButtonComponent.vue";
+import FooterComponent from "../components/FooterComponent.vue";
+import Headings from "../components/Headings.vue";
+
+async function fetchKommuner() {
+  const response = await axios.get("http://localhost:8080/kommuner?regionskode=1084");
+  console.log(response);
+}
+
+async function fecthArtworks() {
+  const response = await axios.get("http://127.0.0.1:8080/artworks");
+  console.log(response);
+}
+</script>
+
+<template>
+  <div>
+    <button @click="fecthArtworks">Fetch Artworks</button>
+
+    <div class="bg-blue-400 flex">
+      <IconButtonComponent variant="instagram" height="25px" />
+      <IconButtonComponent variant="mail" height="25px" />
+      <IconButtonComponent variant="arrow-circle" height="25px" scrollToId="targetElement" />
+    </div>
+
+    <div class="bg-red-300">blablaaaa</div>
+    <p class="p-4 m-10 border-2 text-red-900">hellloooo im styled with tailwindcss</p>
+
+    <Headings :level="1" text="H1"></Headings>
+    <Headings :level="2" text="H2"></Headings>
+    <Headings :level="3" text="H3"></Headings>
+    <div id="scroll-test" style="margin-top: 2000px">Scroll test</div>
+    <FooterComponent></FooterComponent>
+  </div>
+</template>
+
+<style scoped>
+/* Your scoped styles */
+</style>
