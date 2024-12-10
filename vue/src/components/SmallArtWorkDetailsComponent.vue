8a257a07ce570a333c15ab19e84911a5a3c0b8fd
BlablaTara <tara0561@stud.kea.dk>
2024-12-10 16:18:47 +0100

    Slettet pris visning i SmallArtWorkDetailsComponent.vue Når musen altså hover over billedet.
    Har opdateret med de sidst PR (kontaktKnap fra emma)
    
    Beholder denne branch, men går til ny branch, så kan jeg komme tilbage senere og arbejde videre.

--- vue/src/components/SmallArtWorkDetailsComponent.vue
+++ vue/src/components/SmallArtWorkDetailsComponent.vue
@@ -21,7 +21,6 @@ defineProps<{
        class="absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 text-white rounded shadow-lg p-4 flex flex-col justify-center items-center transition-opacity duration-300 opacity-100 pointer-events-none">
     <h3 class="text-lg font-bold mb-2">{{ artWork.title }}</h3>
     <p class="text-sm mb-2">{{ artWork.description }}</p>
-    <p class="text-sm font-semibold">Price: {{ artWork.price }} DKK</p>
   </div>
 </template>
 
