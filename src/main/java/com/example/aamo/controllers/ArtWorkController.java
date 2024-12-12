95cb7b77632ab79ebf277aeba2eadc1ed5eafa08
Nannahp <nannahofgaard@hotmail.com>
2024-12-12 09:44:43 +0100

    Backend create artwork metoder i service + en test controller metode api/saveArtwork

--- src/main/java/com/example/aamo/controllers/ArtWorkController.java
+++ src/main/java/com/example/aamo/controllers/ArtWorkController.java
@@ -1,20 +1,27 @@
 package com.example.aamo.controllers;
 
 import com.example.aamo.models.ArtWork;
+import com.example.aamo.models.ArtWorkTag;
+import com.example.aamo.models.ArtworkDTO;
+import com.example.aamo.models.TagType;
 import com.example.aamo.repositories.ArtWorkRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.aamo.repositories.ArtWorkTagRepository;
+import com.example.aamo.services.ArtworkService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(origins = {"http://127.0.0.1:5173", "http://localhost:5173"})
 @RestController
 public class ArtWorkController {
     private ArtWorkRepository artWorkRepository;
+    private ArtWorkTagRepository artWorkTagRepository;
+    private ArtworkService artworkService;
 
-    public ArtWorkController(ArtWorkRepository artWorkRepository){
+    public ArtWorkController(ArtWorkRepository artWorkRepository, ArtWorkTagRepository artWorkTagRepository, ArtworkService artworkService){
         this.artWorkRepository = artWorkRepository;
+        this.artWorkTagRepository = artWorkTagRepository;
+        this.artworkService = artworkService;
     }
 
 
@@ -27,5 +34,24 @@ public class ArtWorkController {
     @GetMapping("/test")
     public String test() {
         return "Test endpoint is working!";
+    }
+    @GetMapping("api/tags")
+    public List<ArtWorkTag> getTags() {
+        return artWorkTagRepository.findAll();
+    }
+    @PostMapping("api/addArtwork")
+    public void addArtwork(@RequestBody ArtworkDTO artworkDTO) {
+        artworkService.saveArtworkWithTagsAndPicture(artworkDTO);
+    }
+    @GetMapping("api/saveArtwork")
+    public ResponseEntity saveArtwork() {
+        List<ArtWorkTag> tags =  List.of(
+                new ArtWorkTag(TagType.SIZE, "50x50"),
+                new ArtWorkTag(TagType.THEME, "Damer"),
+                new ArtWorkTag(TagType.COLOR, "Rød")
+        );
+        ArtworkDTO artworkDTO = new ArtworkDTO("test", "test",100, false, true, tags, "/eva.png");
+        artworkService.saveArtworkWithTagsAndPicture(artworkDTO);
+        return ResponseEntity.ok("works");
     }
 }
--- src/main/java/com/example/aamo/repositories/ArtWorkTagRepository.java
+++ src/main/java/com/example/aamo/repositories/ArtWorkTagRepository.java
@@ -1,7 +1,11 @@
 package com.example.aamo.repositories;
 
 import com.example.aamo.models.ArtWorkTag;
+import com.example.aamo.models.TagType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ArtWorkTagRepository extends JpaRepository<ArtWorkTag, Integer> {
+    Optional<ArtWorkTag> findByTagTypeAndTagValue(TagType tagType, String tagValue);
 }
