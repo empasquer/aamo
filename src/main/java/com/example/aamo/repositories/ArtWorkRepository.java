54a36c0a27350aed2c1f22b53e5e23c801dfc751
BlablaTara <tara0561@stud.kea.dk>
2024-12-16 18:14:48 +0100

    Pagination, men kun 1 billede af gangen :(

--- src/main/java/com/example/aamo/repositories/ArtWorkRepository.java
+++ src/main/java/com/example/aamo/repositories/ArtWorkRepository.java
@@ -1,6 +1,8 @@
 package com.example.aamo.repositories;
 
 import com.example.aamo.models.ArtWork;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,5 +14,9 @@ public interface ArtWorkRepository extends JpaRepository<ArtWork, Integer> {
     List<ArtWork> findByTypeTrue();
 
     List<ArtWork> findByTypeTrue(Sort sort);
+
+    Page<ArtWork> findByTypeTrue(Pageable pageable);
+
+
 
 }
