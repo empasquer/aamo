8ffa6c362ddd2da4e30ca76ddcd3945092da79cb
Nannahp <nannahofgaard@hotmail.com>
2024-12-30 11:44:32 +0100

    no

--- vue/src/components/EventDetails.vue
+++ vue/src/components/EventDetails.vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { ref, watch, onMounted } from "vue";
+import { ref, watch, onMounted, computed } from "vue";
 import axios from "axios";
 import Carousel from "./Carousel.vue";
 import Headings from "./HeadingsComponent.vue";
 import BodyTextComponent from "./BodyTextComponent.vue";
 
-// Props
 const props = defineProps<{
   eventId: string | number;
 }>();
@@ -22,10 +21,8 @@ const event = ref({
   description: "",
 });
 
-// Computed property for mediaUrls
-const mediaUrls = () => event.value.mediaUrls;
+const mediaUrls = computed(() => event.value.mediaUrls);
 
-// Method to fetch event data
 const fetchEvent = async (eventId: string | number) => {
   try {
     const response = await axios.get(`http://localhost:8080/api/event?eventId=${eventId}`);
@@ -35,15 +32,13 @@ const fetchEvent = async (eventId: string | number) => {
   }
 };
 
-// Watch for changes in `eventId` prop and fetch event data
 watch(
-  () => props.eventId,
-  (newId) => {
-    fetchEvent(newId);
-  }
+    () => props.eventId,
+    (newId) => {
+      fetchEvent(newId);
+    }
 );
 
-// Fetch event data when the component is mounted
 onMounted(() => {
   fetchEvent(props.eventId);
 });
@@ -51,14 +46,16 @@ onMounted(() => {
 
 <template>
   <div class="flex flex-col items-center gap-6 p-4 sm:mt-32">
-    <!-- Event Details -->
-    <Headings :level="3" :text="event.title" />
+    <Headings :level="3" :text="event.title" color="white" />
 
-    <h4 class="font-inter text-[#4A4A4A]">{{ event.formattedStartDate }} - {{ event.formattedEndDate }}</h4>
+    <h4 class="font-inter text-white">
+      {{ event.formattedStartDate }}
+      <span class="text-white" v-if="event.formattedStartDate && event.formattedEndDate"> - </span>
+      {{ event.formattedEndDate }}
+    </h4>
 
-    <!-- Carousel -->
-    <Carousel v-if="mediaUrls() && mediaUrls().length > 0" :media-urls="mediaUrls()" />
+    <Carousel v-if="mediaUrls && mediaUrls.length > 0" :media-urls="mediaUrls" />
 
-    <BodyTextComponent :text-content="event.description" class="sm:w-3/4 w-full text-center sm:leading-8" />
+    <BodyTextComponent color="white" :textContent="event.description" class="sm:w-3/4 w-full text-center sm:leading-8" />
   </div>
 </template>
